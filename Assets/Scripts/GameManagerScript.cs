f29ab7f20d22ae679185c2cd1e76b0b50310a65b
PingusPT <122572061+PingusPT@users.noreply.github.com>
2024-01-28 01:13:00 +0000

    NotFamalyFriendScene

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -10,10 +10,20 @@ public class GameManagerScript : MonoBehaviour
     [Header("Values")]
     [SerializeField] float CorrectAnswers = 0;
     [SerializeField] float CorretasParaGanhar = 10;
+    [SerializeField] int vidasDoZe = 5;
     [Space]
     [Header("Health Bar")]
     [SerializeField] Image heathBar;
-    
+    [Space]
+    [Header("Ze dos Camiões")]
+    [SerializeField]
+    Animator animZe;
+    [SerializeField]
+    Animator Caminhoem;
+    [SerializeField]
+    Animator Roda1;
+    [SerializeField]
+    Animator Roda2;
 
 
     private void Start()
@@ -34,7 +44,26 @@ public class GameManagerScript : MonoBehaviour
 
     public void ChangeHealth(int valuevida)
     {
-        CorrectAnswers += valuevida;
-        heathBar.fillAmount = CorrectAnswers / CorretasParaGanhar;
+        if(valuevida < 0)
+        {
+            if(vidasDoZe <= 0)
+            {
+                Caminhoem.SetTrigger("GoCar");
+                Roda1.SetTrigger("GoRoda");
+                Roda2.SetTrigger("GoRoda");
+            }
+            else
+            {
+                animZe.SetTrigger("TakeDamage");
+                vidasDoZe--;
+            }
+            
+        }
+        else
+        {
+            CorrectAnswers += valuevida;
+            heathBar.fillAmount = CorrectAnswers / CorretasParaGanhar;
+        }
+        
     }
 }
